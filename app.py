c7acbb0652745354cbd90ab9915c3b0184dd71a2
Mahmoud Mohsen <mmohsen35@gmail.com>
2015-07-13 21:16:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from gensim.models import word2vec
 
 from flask import Flask,render_template, request,json,jsonify
 
+#this part to get the absloute path to our model file
 import sys
 from imp import reload
 reload(sys)
 sys.setdefaultencoding('utf8')
+#type here the model name you have during kaggle's tutorial.
 model_name = "300features_40minwords_10context.gensim"
 my_dir = os.path.dirname(__file__)
 model_file_path = os.path.join(my_dir, model_name)
-
 model = word2vec.Word2Vec.load(model_file_path)
 
 app = Flask(__name__)
